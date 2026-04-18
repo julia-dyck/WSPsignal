0067f8aaed85832c20c956087bf1941f200708f7
julia-dyck <julia.alexandra.dyck@gmail.com>
2026-04-18 15:22:31 +0200

    edit docu: add that data is right censored, add refs

--- R/WSPsignal-package.R
+++ R/WSPsignal-package.R
@@ -1,6 +1,6 @@
 #' The 'WSPsignal' package.
 #'
-#' @description An R package to perform Bayesian Weibull Shape Parameter (BWSP)
+#' @description An R package to perform Bayesian and frequentist Weibull Shape Parameter (WSP)
 #' tests for signal detection.
 #'
 #' @docType package
@@ -15,7 +15,12 @@
 #' @importFrom Rdpack reprompt
 #'
 #' @references
+#' \insertRef{sauzet2022}{WSPsignal}
+#' 
+#' \insertRef{sauzet2024}{WSPsignal}
+#' 
+#' \insertRef{dyck2024bpgwsppreprint}{WSPsignal}
+#'
 #' Stan Development Team (NA). RStan: the R interface to Stan. 
 #' R package version 2.32.6. https://mc-stan.org
-#'
 NULL
