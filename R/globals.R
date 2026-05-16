6d113a456ae205113a385879f47837d1525125bd
julia-dyck <julia.alexandra.dyck@gmail.com>
2026-05-16 10:42:49 +0200

    add globalVariables to suppress NOTE from cran check

---
+++ R/globals.R
@@ -0,0 +1,19 @@
+#' @importFrom utils globalVariables
+NULL
+
+utils::globalVariables(c(
+  ".", "AUC", "FNR", "FPR", "FPR_full", "FPR_vec",
+  "N", "TNR", "TPR", "TPR_full", "TPR_vec",
+  "adr.rate", "adr.relsd", "adr.when",
+  "auc", "br", "cred.level",
+  "dist.prior.to.truth", "fnr", "fpr",
+  "ga.po.n_eff", "group", "mod", "n_eff",
+  "nu.po.n_eff", "parameter", "post.ci.type",
+  "prior.belief", "prior.dist", "prior.sd",
+  "reps.notrun", "reps.planned",
+  "res.batch", "res_b", "res_f",
+  "run.min", "sensitivity.option",
+  "spec", "test.type", "tnr",
+  "total.notrun", "total.planned",
+  "tpr", "tte.dist"
+))
